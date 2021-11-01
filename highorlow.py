57c7e24bbaa0395ddfc48f072acbca299c0c1e6c
ben <ben.saxon@hotmail.co.uk>
2021-11-01 10:35:21 +0000

    added

---
+++ highorlow.py
@@ -0,0 +1,23 @@
+import random as r
+gues = -1
+guesses = 0
+
+def guess():
+    try:
+        g = int(input("take a guess between 0 and 100: "))
+        return g
+    except:
+         print("guess not valid try again")
+         guess()
+
+
+
+num = r.randrange(101)
+print(num)
+
+while  gues != num:   
+    guesses = guesses+1
+    print("guess number: ", guesses)
+    gues = guess()
+
+print ("correct guess welldone!")
